0784754fc52e0d9140b4828d2253465a889077bd
bobbosco77 <bobbosco777@gmail.com>
2026-06-07 03:02:07 +0100

    Re-Fix Render allowed hosts

--- countrywide/settings.py
+++ countrywide/settings.py
@@ -27,12 +27,7 @@ SECRET_KEY = 'django-insecure-1gov(ovzz42=*#ekm)qi6!t4$mbr=(b2d2!_(e!80hmu*(gyni
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost',
-    'countrywide-lending.onrender.com',
-]
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
