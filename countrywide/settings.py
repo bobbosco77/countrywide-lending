78ada1db88c568faa71e0b6c6b5ab7fa6f45788e
bobbosco77 <bobbosco777@gmail.com>
2026-06-07 09:31:20 +0100

    enable debug

--- countrywide/settings.py
+++ countrywide/settings.py
@@ -25,9 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1gov(ovzz42=*#ekm)qi6!t4$mbr=(b2d2!_(e!80hmu*(gyni'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'countrywide-lending.onrender.com',
+]
 
 # Application definition
 
